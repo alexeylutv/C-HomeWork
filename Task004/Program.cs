ea61f69293cc29d684c6d3c56f7d980d30c512cf
Alex <l3lik@list.ru>
2023-07-10 22:16:46 +0300

    some solutions to hw

---
+++ Task004/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите три числа: ");
+int numberA = int.Parse (Console.ReadLine());
+int numberB = int.Parse (Console.ReadLine());
+int numberC = int.Parse (Console.ReadLine());
+
+int maxNumber = numberA;
+if (numberA < numberB) maxNumber = numberB;
+if (numberA > numberB) maxNumber = numberA;
+if (numberC > maxNumber) maxNumber = numberC;
+
+Console.Write ("Максимальное число: ");
+Console.WriteLine (maxNumber);
---
+++ Task008/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число N: ");
+int numberN = int.Parse (Console.ReadLine());
+Console.WriteLine("Четные числа от 1 до " + numberN + ":");
+{
+for (int i = 2; i<= numberN; i+=2)
+Console.WriteLine(i);
+}
